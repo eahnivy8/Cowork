6b6c724a6fabc158a1c03f7007fb837cd371c4c2
edahn7 <edahn7@bodyfriend.co.kr>
2022-04-05 20:54:46 +0900

    aaa

--- Coworktest/ViewController.swift
+++ Coworktest/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
 
 
 }
